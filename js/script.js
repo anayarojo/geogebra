687dffb2ddc8321226470dbd563a6f1790a3c35d
anayarojo <raulmartin_04@hotmail.com>
2019-01-29 14:16:29 -0700

    Implement examples

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const strUrl = window.location.href;
 const objUrl = new URL(strUrl);
 const type = objUrl.searchParams.get('type');
+const example = objUrl.searchParams.get('example');
 
 var types = ['classic', 'graphing', 'geometry', '3d'];
 
@@ -8,20 +9,8 @@ if(type == undefined || !types.includes(type)){
     window.location.replace('/?type=graphing');
 }
 
-function addOnGeogrebra(objectName){
-    let ggb = window['ggbApplet'];
-    let type = ggb.getObjectType(objectName);
-    console.log(type);
-
-    if(type == 'point') {
-        let coordX = ggb.getXcoord(objectName);
-        let coordY = ggb.getYcoord(objectName);
-        ggb.setCoords(objectName, coordX, coordY);
-    }
-};
-
 var callback = function(api){
-    api.registerAddListener('addOnGeogrebra');
+    //api.registerAddListener('addOnGeogrebra');
 };
 
 var parameters = {};
@@ -65,48 +54,43 @@ else
         // "buttonRounding":0.7,
         // "buttonShadows":false,
     };
-   
-    /*
-   parameters = {
-        "id": "ggbApplet",
-        "width":1920,
-        "height":825,
-        "showMenuBar":false,
-        "showAlgebraInput":true,
-        "showToolBar":true,
-        "customToolBar":"0 77 73 62 | 1 501 67 , 5 19 , 72 75 76 | 2 15 45 , 18 65 , 7 37 | 4 3 8 9 , 13 44 , 58 , 47 | 16 51 64 , 70 | 10 34 53 11 , 24  20 22 , 21 23 | 55 56 57 , 12 | 36 46 , 38 49  50 , 71  14  68 | 30 29 54 32 31 33 | 25 17 26 60 52 61 | 40 41 42 , 27 28 35 , 6",
-        "showToolBarHelp":true,
-        "showResetIcon":false,
-        "enableLabelDrags":false,
-        "enableShiftDragZoom":true,
-        "enableRightClick":false,
-        "errorDialogsActive":false,
-        "useBrowserForJS":false,
-        "allowStyleBar":false,
-        "preventFocus":false,
-        "showZoomButtons":true,
-        "capturingThreshold":3,
-        "appletOnLoad":function(api){},
-        "showFullscreenButton":true,
-        "scale":1,
-        "disableAutoScale":false,
-        "allowUpscale":false,
-        "clickToLoad":false,
-        "appName":"graphing",
-        "showSuggestionButtons":true,
-        "buttonRounding":0.7,
-        "buttonShadows":false,
-        "language":"es",
-   };
-   */
 }
 
-var views = {'is3D': 0,'AV': 1,'SV': 0,'CV': 0,'EV2': 0,'CP': 0,'PC': 0,'DA': 0,'FI': 0,'macro': 0};
-var ggbApp = new GGBApplet(parameters, '5.0', views);
+if(example != undefined){
+    $.ajax({
+        url: 'js/examples.json',
+        dataType: 'json',
+        success: function(data){
+            console.log(data);
 
-window.addEventListener("load", function () {
-    // ggbApp.setHTML5Codebase("https://cdn.geogebdra.org/apps/5.0.498.0/web3d");
-    // ggbApp.setHTML5Codebase('js/lib/GeoGebra/HTML5/5.0/web3d/', true);
-    ggbApp.setHTML5Codebase('js/lib/GeoGebra/bundle-5-0-523-0/HTML5/5.0/web3d/', true);
-    ggbApp.inject('ggb-element');
-});
+            var obj = data.find(function(item){
+                return item.example == example;
+            });
+
+            if(obj) parameters.ggbBase64 = obj.base64;
+            
+            initGeoGebra(parameters);
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.error(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        }
+    });
+}
+else
+{
+    initGeoGebra(parameters);
+}
+
+function initGeoGebra(parameters){
+
+    var views = {'is3D': 0,'AV': 1,'SV': 0,'CV': 0,'EV2': 0,'CP': 0,'PC': 0,'DA': 0,'FI': 0,'macro': 0};
+    var ggbApp = new GGBApplet(parameters, '5.0', views);
+
+    window.addEventListener("load", function () {
+        ggbApp.setHTML5Codebase('js/lib/GeoGebra/bundle-5-0-523-0/HTML5/5.0/web3d/', true);
+        ggbApp.inject('ggb-element');
+    });
+}
+
+
+
